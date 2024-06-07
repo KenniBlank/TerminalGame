964dbe769c82277953e351d13618d0986ef6ab1e
KenniBlank <keniblank@gmail.com>
2024-06-07 19:22:48 +0545

    someLifeImprovementChanges

--- obj/Debug/net8.0/SnakeGame-in-C-.AssemblyInfo.cs
+++ obj/Debug/net8.0/SnakeGame-in-C-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakeGame-in-C-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e1395a567328550ba3ce8ce7a58fceeef8259d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc000e8b505e3aa2ce76174c91d6ce3d5f30360")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakeGame-in-C-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakeGame-in-C-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
